0885c25e3299f4a6fae29be322ad3296720d8e84
gerasymchuk <mgerasimchuk95@gmail.com>
2026-03-01 00:18:34 +0200

    create FastAPI app

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from reviews-analyzer!")
+import uvicorn
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(name)s | %(levelname)s | %(message)s"
+)
+
+from fastapi import FastAPI
+from app.api.routes import router
+
+app = FastAPI(title="Reviews Analyzer API")
+app.include_router(router)
+
+
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, log_level="info")
